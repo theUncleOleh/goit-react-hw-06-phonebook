9c2a758c1224e8365bd59c67cfa572f1011d7c40
Oleh Semennykov <sumcue@gmail.com>
2022-05-27 15:41:29 +0300

    add lazy load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { lazy, Suspense } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import Layout from './Layout/Layout';
-import PhoneBookPage from '../pages/PhoneBookPage';
-import SecondPage from '../pages/SecondPage';
-import NotFoundPage from '../pages/NotFoundPage';
-
+import Loader from '../components/Loader/Loader';
+// import PhoneBookPage from '../pages/PhoneBookPage';
+// import SecondPage from '../pages/SecondPage';
+// import NotFoundPage from '../pages/NotFoundPage';
+const PhoneBookPage = lazy(() => import('../pages/PhoneBookPage.jsx'));
+const SecondPage = lazy(() => import('../pages/SecondPage.jsx'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage.jsx'));
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<PhoneBookPage />} />
-          <Route path="nextpage" element={<SecondPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<PhoneBookPage />} />
+            <Route path="nextpage" element={<SecondPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
